2e413081b2442ee669a21161f577eae6a90e4788
Josan Mihai <mihai.josan12@gmail.com>
2024-09-09 10:47:10 +0300

    setup clerk sign in and sign up pages

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const SetupPage = () => {
+    return (
+        <div className="p-4">
+            This is a proetcted route!
+        </div>
+    )
+}
+
+export default SetupPage
