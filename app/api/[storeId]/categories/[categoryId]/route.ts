275bba9ba6e20e7f71f3a54104a8debc9712f810
Josan Mihai <mihai.josan12@gmail.com>
2024-09-20 20:16:09 +0300

    Implemented stripe in the project

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -16,6 +16,9 @@ export async function GET(
             where: {
                 id: params.categoryId,
             },
+            include: {
+                billboard: true,
+            },
         })
 
         return NextResponse.json(category)
@@ -69,7 +72,7 @@ export async function PATCH(
             },
             data: {
                 name,
-                billboardId
+                billboardId,
             },
         })
 
